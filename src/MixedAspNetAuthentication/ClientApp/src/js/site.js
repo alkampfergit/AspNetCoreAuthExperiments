8824cdae58df7fb15362db64bc295ce05a420612
Gian Maria Ricci <ricci.gianmaria@gmail.com>
2022-05-27 15:29:11 +0200

    Added webpack

---
+++ src/MixedAspNetAuthentication/ClientApp/src/js/site.js
@@ -0,0 +1,19 @@
+﻿// this started with the tutorial: https://dev.to/larswillemsens/managing-asp-net-core-mvc-front-end-dependencies-with-npm-and-webpack-part-1-3jf5
+// JS Dependencies: Popper, Bootstrap & JQuery
+import '@popperjs/core';
+import 'bootstrap';
+import 'jquery';
+// Using the next two lines is like including partial view _ValidationScriptsPartial.cshtml
+import 'jquery-validation';
+import 'jquery-validation-unobtrusive';
+
+// CSS Dependencies: Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Custom JS imports
+// ... none at the moment
+
+// Custom CSS imports
+import '../css/site.css';
+
+console.log('The \'site\' bundle has been loaded!');
---
+++ src/MixedAspNetAuthentication/ClientApp/webpack.config.js
@@ -0,0 +1,25 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: {
+        site: './src/js/site.js'
+    },
+    output: {
+        filename: '[name].entry.js',
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
+                type: 'asset'
+            },
+        ]
+    }
+};
